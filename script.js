2f90311871c8ec18bbdb7acb3760aae94d071bb0
unknown <BT_3W2_13@BKTECH.LOCAL>
2024-10-18 15:21:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+numStudents=0
+autoC = false
+
+
+
+function increment(){
+    numStudents++
+    studentDis = document.getElementById("count-el")
+    studentDis.innerHTML = numStudents
+}
+function save(){
+    saves = document.getElementById("save-atmp")
+    saves.innerHTML += numStudents + " - "
+}
+let intervalId;
+
+function auto() {
+    autoC = !autoC;
+
+    if (autoC) {
+
+        intervalId = setInterval(increment, 250);
+    } else {
+
+        clearInterval(intervalId);
+    }
+}
+
+
+
+
+
+
+//Title = Student Counter
+
+
+
+
